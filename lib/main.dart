cc10cc09b63fb0d740ea38fb5084abfaece06bd1
anne.quiedeville <anne.quiedeville@dotsafe.fr>
2020-05-26 11:28:15 +0200

    Added button to add custom name

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -27,8 +30,12 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();
-  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0, color: Colors.purple);
+  final List<String> _ownSaved = <String>[];
+  final List<WordPair> _saved = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0, color: Colors.deepPurpleAccent);
+  final TextStyle _titleStyle = const TextStyle(fontSize: 22.0, color: Colors.deepPurpleAccent, fontWeight: FontWeight.bold);
+  final GlobalKey<AnimatedListState> _listKey= GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,36 +48,143 @@ class RandomWordsState extends State<RandomWords> {
       body: _buildSuggestions(),
     );
   }
+
   void _pushSaved() {
     Navigator.of(context).push(
       MaterialPageRoute<void>(
         builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map(
-              (WordPair pair) {
-                return ListTile(
-                  title: Text(
-                    pair.asPascalCase,
-                    style: _biggerFont,
-                  ),
-                );
-              }
-          );
-          final List<Widget> divided = ListTile
-          .divideTiles(
-            context: context,
-            tiles: tiles,
-          )
-          .toList();
           return Scaffold(
             appBar: AppBar(
-              title: Text('Saved suggestions'),
+              title: Text('Saved Names'),
             ),
-            body: ListView(children: divided),
+            body: _buildLists(),
+            floatingActionButton: FloatingActionButton(
+              onPressed: _addCustomElement,
+              child: Icon(Icons.add),
+              backgroundColor: Colors.deepPurple,
+            ),
           );
         }
       )
     );
   }
+
+  Widget _buildLists() {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(8.0),
+        ),
+        Text(
+          'My custom names',
+          style: _titleStyle,
+        ),
+        Expanded(
+          child:
+            _buildOwnSaved(),
+        ),
+         Text(
+          'Suggestions I saved',
+          style: _titleStyle,
+        ),
+        Expanded(
+          child: _buildSaved(),
+        ),
+      ],
+    );
+  }
+
+  Widget _addCustomElement()
+  {
+    final myController = TextEditingController();
+    final _formKey = GlobalKey<FormState>();
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Stack(
+              overflow: Overflow.visible,
+              children: <Widget>[
+                Positioned(
+                  right: -40.0,
+                  top: -40.0,
+                  child: InkResponse(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: CircleAvatar(
+                      child: Icon(Icons.close),
+                      backgroundColor: Colors.deepPurple,
+                    ),
+                  ),
+                ),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          controller: myController,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: RaisedButton(
+                          child: Text('Add'),
+                          onPressed: () {
+                            if (_formKey.currentState.validate()) {
+                              _formKey.currentState.save();
+                              _ownSaved.insert(0, myController.text);
+                              _listKey.currentState.insertItem(0);
+                              Navigator.of(context, rootNavigator: true).pop();
+                            }
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
+  Widget _buildOwnSaved() {
+    return AnimatedList(
+        key: _listKey,
+        initialItemCount: _ownSaved.length,
+        itemBuilder: (context, index, animation) {
+          return _buildItem(_ownSaved[index], animation);
+        },
+    );
+  }
+
+  Widget _buildItem(String item, Animation animation) {
+    return SizeTransition(
+      sizeFactor: animation,
+      child: Card(
+        child: ListTile(
+          title: Text(
+            item,
+            style: _biggerFont,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSaved() {
+    return AnimatedList(
+      initialItemCount: _saved.length,
+      itemBuilder: (context, index, animation) {
+        return _buildItem(_saved[index].asPascalCase, animation);
+      },
+    );
+  }
+
   Widget _buildSuggestions() {
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
@@ -84,6 +198,7 @@ class RandomWordsState extends State<RandomWords> {
       }
     );
   }
+
   Widget _buildRow(WordPair pair) {
     final bool alreadySaved = _saved.contains(pair);
     return ListTile(
@@ -93,7 +208,7 @@ class RandomWordsState extends State<RandomWords> {
       ),
       trailing: Icon(
         alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.purpleAccent : null,
+        color: alreadySaved ? Colors.deepPurpleAccent : null,
       ),
       onTap: () {
         setState(() {
@@ -107,3 +222,5 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 }
+
+
